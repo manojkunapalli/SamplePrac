7c48cdba0e25b25a9cbf954a2a43144f41722c02
manojkunapalli <manojkunapalli@gmail.com>
2020-10-09 10:59:52 +0530

    Added: Even ODD functionality

--- src/sequence.java
+++ src/sequence.java
@@ -6,5 +6,12 @@ public class sequence {
         Scanner input = new Scanner(System.in);
         int t = input.nextInt();
         System.out.println("Given input is "+t);
+
+        // check even or odd
+        if(t%2 == 0){
+            System.out.println(t+" is even number");
+        } else {
+            System.out.println(t+" is odd number ");
+        }
     }
 }
